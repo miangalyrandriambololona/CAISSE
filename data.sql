e3f55d427c3d28f1f1cd1d21904c51106392e5a9
miangalyrandriambololona <rianamiangaly7@gmail.com>
2026-03-31 09:44:48 +0300

    .

---
+++ data.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO produit (designation, prix, quantite_en_stock) VALUES 
+('Pain', 500, 20),
+('Sucre 1kg', 3000, 50),
+('Eau vive', 1500, 100),
+('Biscuits', 1000, 40),
+('Savon', 800, 60);
+
+INSERT INTO caisse (numero_caisse) VALUES 
+('Caisse 01'), 
+('Caisse 02');
